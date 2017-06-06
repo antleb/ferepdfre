1f4c8df74d733e5681dc8af95b827415fb7d6cb1
Katerina Gkirtzou <katerina.gkirtzou@gmail.com>
2017-06-06 18:42:27 +0300

    Change reading files function v2

--- src/main/java/eu/dnetlib/UpdateIndex.java
+++ src/main/java/eu/dnetlib/UpdateIndex.java
@@ -4,9 +4,13 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.FileSystems;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.Properties;
+import java.util.stream.Stream;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
@@ -15,11 +19,10 @@ import org.slf4j.LoggerFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 
-import eu.dnetlib.elasticsearch.ElasticSearchConfiguration;
-import eu.dnetlib.utils.ExtensionResolver;
 import eu.openminted.content.index.IndexConfiguration;
 import eu.openminted.content.index.IndexPublication;
 import eu.openminted.content.index.entities.Publication;
+import eu.openminted.content.index.entities.utils.ExtensionResolver;
 import eu.openminted.omtdcache.CacheDataIDSHA1;
 
 /**
@@ -46,72 +49,74 @@ public class UpdateIndex {
         
         // Read json files
         log.info("Read documents from json files");
+        int counter = 0;
         ObjectMapper mapper = new ObjectMapper();
         Path p = FileSystems.getDefault().getPath(pathToFiles + "metadata/");
-		Files.walk(p).forEach(filePath -> {
+        Stream<Path> walk = Files.walk(p);     
+        Iterator<Path> iterPath = walk.iterator();
+        
+        while(iterPath.hasNext()) {
+        		Path filePath = iterPath.next();
 		        if (Files.isRegularFile(filePath)) {
 		            //Do something with filePath
 			        log.info("Loading publication from json file " + filePath);
 			        File workingFile = filePath.toFile();
 			        Publication pub;
-					try {
-						pub = mapper.readValue(workingFile, Publication.class);
-					
-						String pubID = pub.getOpenaireId();
-						log.info("Loaded publication :: " + pub.toString());
+			        pub = mapper.readValue(workingFile, Publication.class);
+						
+					String pubID = pub.getOpenaireId();
+					log.info("Loaded publication :: " + pub.toString());
 										
 					
-						// Recalculate hash value
-						File fileOld = new File(pub.getPathToFile());				
-						String hashValueSHA1;
-					
-						hashValueSHA1 = sha1Calc.getID(FileUtils.readFileToByteArray(fileOld));
-						pub.setHashValue(hashValueSHA1);
-					
-					
-					
-						// Optimize layout in directories
-						// Create store folder(s)
-						String storePrefix = pubID.substring(0, pubID.lastIndexOf("::"));
-						String id = pubID.substring(pubID.lastIndexOf("::")+2); 
-						File storeFolder = new File(pathToFiles + storePrefix + "/" + id.substring(0, 3));
-						if (!storeFolder.exists()) {
-							storeFolder.mkdirs();
-						}
-						// Move to new store folder
-						String extension = ExtensionResolver.getExtension(pub.getMimeType());
+					// Recalculate hash value
+					File fileOld = new File(pub.getPathToFile());				
+					String hashValueSHA1;
 						
-						String filenameNew = storeFolder.getPath() + "/" + pubID + extension;
-						File fileNew = new File(filenameNew);
-						try {			
-						    FileUtils.copyFile(fileOld, fileNew);
-						    fileOld.delete();
-						} catch (IOException e) {
-						    e.printStackTrace();
-						}
-						// Update new paths
-						pub.setPathToFile(fileNew.getCanonicalPath());
-						pub.setUrl(urlDomain + filenameNew);
-									
-						log.info("Updated publication ::" + pub.toString());
-						// Update publication in index
-						boolean success = index.addPublication(pub, pubID); 
-						log.info("Succeded? " + success);
-						assert(success);
-						
-						// Export Publication in json and save to disk
-					    Gson gson = new Gson();
-					    FileWriter fw = new FileWriter(pathToFiles + "metadata_new/" + pubID + ".json");
-					    gson.toJson(pub, fw);
-					    fw.close();
-					    
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
+					hashValueSHA1 = sha1Calc.getID(FileUtils.readFileToByteArray(fileOld));
+					pub.setHashValue(hashValueSHA1);
+					
+					
+					
+					// Optimize layout in directories
+					// Create store folder(s)
+					String storePrefix = pubID.substring(0, pubID.lastIndexOf("::"));
+					String id = pubID.substring(pubID.lastIndexOf("::")+2); 
+					File storeFolder = new File(pathToFiles + storePrefix + "/" + id.substring(0, 3));
+					if (!storeFolder.exists()) {
+						storeFolder.mkdirs();
 					}
+					// Move to new store folder
+					String extension = ExtensionResolver.getExtension(pub.getMimeType());
+						
+					String filenameNew = storeFolder.getPath() + "/" + pubID + extension;
+					File fileNew = new File(filenameNew);
+					try {			
+						FileUtils.copyFile(fileOld, fileNew);
+						fileOld.delete();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					// Update new paths
+					pub.setPathToFile(fileNew.getCanonicalPath());
+					pub.setUrl(urlDomain + filenameNew);
+					
+					log.info("Updated publication ::" + pub.toString());
+					// Update publication in index
+					boolean success = index.addPublication(pub, pubID); 
+					log.info("Succeded? " + success);
+					assert(success);
+						
+					// Export Publication in json and save to disk
+					Gson gson = new Gson();
+					FileWriter fw = new FileWriter(pathToFiles + "metadata_new/" + pubID + ".json");
+					gson.toJson(pub, fw);
+					fw.close();
+					counter++;
+					    
+				
 			        
 		        }
-		});
+		}
         
         /*
 		
@@ -176,14 +181,14 @@ public class UpdateIndex {
 			}											
 		}
         */
-		log.info("Updated all documents.");
+		log.info("Updated " + counter + " documents.");
         index.disconnect();
 	}
 	
 	private static String getDomainURL() throws IOException {
          
          Properties configFile = new Properties();
-         configFile.load(FerePdfRe.class.getClassLoader().getResourceAsStream("publicationConfig.properties"));
+         configFile.load(UpdateIndex.class.getClassLoader().getResourceAsStream("publicationConfig.properties"));
 		
          String domainURL = configFile.getProperty("domainURL");
          if (domainURL != null) {
@@ -196,7 +201,7 @@ public class UpdateIndex {
     
     private static String getPathToFiles() throws IOException {
          Properties configFile = new Properties();
-         configFile.load(ElasticSearchConfiguration.class.getClassLoader().getResourceAsStream("publicationConfig.properties"));
+         configFile.load(UpdateIndex.class.getClassLoader().getResourceAsStream("publicationConfig.properties"));
          
          String pathToFiles = configFile.getProperty("pathToFiles");
          if (pathToFiles != null) {
